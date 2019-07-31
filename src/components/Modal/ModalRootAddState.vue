d4b89ac6134827ddc6a84fb39592be18e80e4396
Lewis Farrow <lewis@hifi.agency>
2019-07-31 16:39:54 +0100

    Change copy to explain "create blank" state will not necessarily create blank sub-blocks

--- src/components/Modal/ModalRootAddState.vue
+++ src/components/Modal/ModalRootAddState.vue
@@ -6,6 +6,7 @@
     <template slot="content">
       <p>Do you want to overwrite the current state or save as a new state?</p>
       <p><strong>WARNING:</strong> Any changes within sub-blocks will cause their appropriate state to be overwritten with the content in this current state!</p>
+      <p><strong>IF CREATING NEW BLANK STATE:</strong> Any sub-blocks which use non-inline data (i.e. using a state) will need to be created and saved manually. They will not be blank</p>
       <template>
         <label class="modal-root-add-state__input">
           <input class="modal-root-add-state__input__control modal-root-add-state__input__control--text" type="text" v-model="name" placeholder='e.g. "longDescription", "empty"' @input="validateFunction(name)" v-on:keyup.enter="addFunction(true,name);"/>
